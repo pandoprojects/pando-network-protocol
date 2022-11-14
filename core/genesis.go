50d1c13a2131f43094f3f2051bf0fb8cece0faf6
loganpando <chandan@pandolab.io>
2022-11-14 12:30:40 +0530

    testnet config modification

--- core/genesis.go
+++ core/genesis.go
@@ -1,9 +1,9 @@
 package core
 
 const (
-	MainnetChainID = "pandonet"
+	MainnetChainID = "privatenet"
 
-	MainnetGenesisBlockHash = "0x8984aee9380ed6d0af14939b7886732e760c1c3d2d3e6bfc1e617ec00471a4c9"
+	MainnetGenesisBlockHash = "" // add genesis hash here to run chain
 
 	GenesisBlockHeight = uint64(0)
 )
